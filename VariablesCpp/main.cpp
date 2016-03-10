ea98a9857b4e1e5744d7852c3f70bd04688aa45d
neto <neto@neto-305V4A-305V5A>
2016-03-10 11:27:02 -0600

    LISTO

---
+++ VariablesCpp/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: neto
+ *
+ * Created on 10 de marzo de 2016, 11:06 AM
+ */
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int num = 34;
+int main(int argc, char** argv) {
+    cout<< "el numero es: "<< num<<"\n\n"<< endl;
+    //system("PAUSE"); DÈSCOMENTA EN WINDOWS
+
+    return 0;
+}
+
---
+++ VariablesCpp/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=variablescpp
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/variablescpp
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=variablescpp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/variablescpp.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=variablescpp
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/variablescpp
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=variablescpp.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/variablescpp.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
