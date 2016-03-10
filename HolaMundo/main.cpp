7ee2787aa8c9957d6a04e9478cdc9c1b811a3fcf
neto <neto@neto-305V4A-305V5A>
2016-03-09 21:24:16 -0600

    incial

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: neto
+ *
+ * Created on 9 de marzo de 2016, 09:11 PM
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hola mundo");
+   // system("PAUSE"); descomentar en windows
+    return 0;
+}
+
